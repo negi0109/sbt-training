3139c050924e6c3116e32945e277bb328b73c2e2
negi0109 <negi0109.seg@gmail.com>
2020-03-12 19:09:54 +0900

    不要箇所のコメントアウト

--- src/test/scala/HelloWorldTest.scala
+++ src/test/scala/HelloWorldTest.scala
@@ -3,6 +3,6 @@ import org.scalatest.FunSuite
 class HelloWorldTest extends FunSuite {
     test("HelloWorld.sum") {
         assert(HelloWorld.sum(1, 3) == 4)
-        assert(HelloWorld.sum(1, 3) == 6)
+        // assert(HelloWorld.sum(1, 3) == 6)
     }
 }
