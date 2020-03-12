2931f923746ce72bc8e1bf8d5a0143b8aa2155af
negi0109 <negi0109.seg@gmail.com>
2020-03-12 19:08:58 +0900

    FunSuiteテスト書いてみる

--- build.sbt
+++ build.sbt
@@ -2,3 +2,5 @@ name := "HelloWorldProject"
 version := "1.0"
 
 scalaVersion := "2.12.10"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8"
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,9 @@
 object HelloWorld{
-    def main(args: Array[String]):Unit = {
+    def main (args: Array[String]):Unit = {
         println("Hello,world!")
+    }
+
+    def sum (a: Int,b: Int):Int = {
+        a + b
     }
 }
